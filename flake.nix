ec5a687f4615c6ee9ac8fe9df8b615e5886afd89
Jakob Ankarhem <jakob@ankarhem.dev>
2025-09-16 00:25:55 +0200

    add nix fmt to pre commit hook

--- flake.nix
+++ flake.nix
@@ -11,12 +11,19 @@
     git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = inputs:
+  outputs =
+    inputs:
     let
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (system:
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
           f rec {
             pkgs = import inputs.nixpkgs {
               inherit system;
@@ -29,6 +36,8 @@
             pre-commit-hooks = inputs.git-hooks.lib.${system}.run {
               src = ./.;
               hooks = {
+                nixfmt-rfc-style.enable = true;
+
                 rustfmt = {
                   enable = true;
                   packageOverrides.cargo = pkgs.cargo;
@@ -42,82 +51,103 @@
                 };
               };
             };
-          });
-    in {
+          }
+        );
+    in
+    {
       overlays.default = final: prev: {
-        rustToolchain = let rust = prev.rust-bin;
-        in if builtins.pathExists ./rust-toolchain.toml then
-          rust.fromRustupToolchainFile ./rust-toolchain.toml
-        else if builtins.pathExists ./rust-toolchain then
-          rust.fromRustupToolchainFile ./rust-toolchain
-        else
-          rust.stable.latest.default.override {
-            extensions = [ "rust-src" "rustfmt" ];
-          };
+        rustToolchain =
+          let
+            rust = prev.rust-bin;
+          in
+          if builtins.pathExists ./rust-toolchain.toml then
+            rust.fromRustupToolchainFile ./rust-toolchain.toml
+          else if builtins.pathExists ./rust-toolchain then
+            rust.fromRustupToolchainFile ./rust-toolchain
+          else
+            rust.stable.latest.default.override {
+              extensions = [
+                "rust-src"
+                "rustfmt"
+              ];
+            };
       };
 
-      packages = forEachSupportedSystem ({ pkgs, naerskLib, ... }: rec {
-        hitster = naerskLib.buildPackage {
-          pname = "hitster";
-          src = ./.;
-          buildInputs = with pkgs; [ rustToolchain openssl sqlx-cli ];
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-        };
-        default = hitster;
+      packages = forEachSupportedSystem (
+        { pkgs, naerskLib, ... }:
+        rec {
+          hitster = naerskLib.buildPackage {
+            pname = "hitster";
+            src = ./.;
+            buildInputs = with pkgs; [
+              rustToolchain
+              openssl
+              sqlx-cli
+            ];
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+          };
+          default = hitster;
 
-        dockerImage = pkgs.dockerTools.buildLayeredImage {
-          name = "hitster";
-          tag = "latest";
-          created = "now";
+          dockerImage = pkgs.dockerTools.buildLayeredImage {
+            name = "hitster";
+            tag = "latest";
+            created = "now";
 
-          contents = [
-            # Runtime dependencies
-            pkgs.cacert
-            pkgs.openssl
-            pkgs.sqlite
-            # Application
-            hitster
-          ];
+            contents = [
+              # Runtime dependencies
+              pkgs.cacert
+              pkgs.openssl
+              pkgs.sqlite
+              # Application
+              hitster
+            ];
 
-          config = {
-            Entrypoint = [ "${hitster}/bin/hitster" ];
-            ExposedPorts = { "3000/tcp" = { }; };
-            Env = [
+            config = {
+              Entrypoint = [ "${hitster}/bin/hitster" ];
+              ExposedPorts = {
+                "3000/tcp" = { };
+              };
+              Env = [
                 "HITSTER_DATABASE__PATH=/data/db/hitster.db"
                 "HITSTER_CONFIG_DIR=/config"
+              ];
+              WorkingDir = "/data";
+              Volumes = {
+                "/data" = { };
+              };
+            };
+          };
+        }
+      );
+
+      devShells = forEachSupportedSystem (
+        { pkgs, pre-commit-hooks, ... }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustToolchain
+              openssl
+              pkg-config
+              cargo-deny
+              cargo-edit
+              cargo-watch
+              rust-analyzer
+
+              sqlx-cli
+              dive
             ];
-            WorkingDir = "/data";
-            Volumes = { "/data" = { }; };
+
+            buildInputs = [ pre-commit-hooks.enabledPackages ];
+            shellHook = ''
+              ${pre-commit-hooks.shellHook}
+            '';
+
+            env = {
+              DATABASE_URL = "sqlite://./db/hitster.db";
+              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+            };
           };
-        };
-      });
-
-      devShells = forEachSupportedSystem ({ pkgs, pre-commit-hooks, ... }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            rustToolchain
-            openssl
-            pkg-config
-            cargo-deny
-            cargo-edit
-            cargo-watch
-            rust-analyzer
-
-            sqlx-cli
-            dive
-          ];
-
-          buildInputs = [ pre-commit-hooks.enabledPackages ];
-          shellHook = ''
-            ${pre-commit-hooks.shellHook}
-          '';
-
-          env = {
-            DATABASE_URL = "sqlite://./db/hitster.db";
-            RUST_SRC_PATH =
-              "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
-          };
-        };
-      });
+        }
+      );
     };
 }
